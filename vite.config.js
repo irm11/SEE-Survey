8eadc8c38089ea45f58664792713c4cdeb80d5d4
Yasmeen Ahmed Mohamed (EXT-Nokia) <yasmeen.ahmed_mohamed.ext@nokia.com>
2025-06-03 17:07:17 +0300

    handle access for login

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,7 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
-  server: {
-    port: 3000,
-    open: true,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8000',
-    //     changeOrigin: true,
-    //     secure: false
-    //   }
-    // }
-  },
+  // server: {
+  //   port: 3000
+  // }
 })
